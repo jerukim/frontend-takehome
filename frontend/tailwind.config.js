273d51bf24f6cb78885f5a6b713bed3be8ca67d7
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 17:08:26 -0400

    font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      white: '#F2F1F0',
-      gray: '#AEADAD',
-      black: '#0F0E0C',
-      green: '#4BC2A3',
-      red: '#FF5A44',
+    extend: {
+      fontFamily: {
+        mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        white: '#F2F1F0',
+        gray: '#AEADAD',
+        black: '#0F0E0C',
+        green: '#4BC2A3',
+        red: '#FF5A44',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
