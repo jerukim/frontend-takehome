c4e18cb2e223759c73f748e994f3416d3d00a072
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 18:42:32 -0400

    xxs font size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono],
+        mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
+      },
+      fontSize: {
+        xxs: "0.625rem",
       },
       colors: {
-        white: '#F2F1F0',
-        gray: '#AEADAD',
-        black: '#0F0E0C',
-        green: '#4BC2A3',
-        red: '#FF5A44',
+        white: "#F2F1F0",
+        gray: "#AEADAD",
+        black: "#0F0E0C",
+        green: "#4BC2A3",
+        red: "#FF5A44",
       },
     },
   },
   plugins: [],
-}
+};
