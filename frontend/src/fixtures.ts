d8be0f58d74f021225d3e5e72f4a00ee88cff5ac
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 18:43:00 -0400

    asset fixture uses random number generation

--- frontend/src/fixtures.ts
+++ frontend/src/fixtures.ts
@@ -1,11 +1,20 @@
 import { Asset } from "./types";
 
+function getRandomArbitrary(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
 export const asset: Asset = {
+  img_url: "/img/bitcoin.svg",
   symbol: "BTC",
   name: "Bitcoin",
-  price: "31,119.01",
-  "24h_change": "+22.3 USDC (+7.5$)",
-  "1h_funding": "0.00012%",
-  long_open_interest: "8.871 BTC",
-  short_open_interest: "8.871 BTC",
+  price: getRandomArbitrary(10_000, 60_000),
+  "24h_change": (() => {
+    const num = getRandomArbitrary(-100, 100);
+    const sign = num > 0 ? "+" : "";
+    return `${sign}${num.toFixed(1)} USDC (${sign}${(num / 3).toFixed(1)}$)`;
+  })(),
+  "1h_funding": parseFloat(getRandomArbitrary(-1, 1).toFixed(5)),
+  long_open_interest: parseFloat(getRandomArbitrary(-10, 10).toFixed(3)),
+  short_open_interest: parseFloat(getRandomArbitrary(-10, 10).toFixed(3)),
 };
