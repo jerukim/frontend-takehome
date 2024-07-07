d65b4813e7ffd5f283c6a9c31200b0ad2806ecb2
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 15:17:12 -0400

    base colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#F2F1F0',
+      gray: '#AEADAD',
+      black: '#0F0E0C',
+      green: '#4BC2A3',
+      red: '#FF5A44',
+    }
     extend: {},
   },
   plugins: [],
