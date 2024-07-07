21fd10306db173e73c81058fcee4511bc138aade
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 19:10:30 -0400

    nav items (#6)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import AssetOverview from "./components/AssetOverview";
+import Nav from "./components/Nav";
 import Search from "./components/Search";
 
 import { asset } from "./fixtures";
@@ -9,8 +10,9 @@ function App() {
       <header className="col-span-full grid grid-cols-subgrid">
         <Search />
       </header>
-      <main className="col-span-full grid grid-cols-subgrid">
+      <main className="col-span-full grid grid-cols-subgrid gap-4">
         <AssetOverview asset={asset} />
+        <Nav />
       </main>
     </>
   );
