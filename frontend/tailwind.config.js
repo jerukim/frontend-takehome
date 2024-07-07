7bd2e593b9ef02c3a5a430a9d018726daccf8a3d
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 15:17:58 -0400

    Feature/frontend setup (#1)
    * replace cra with vite
    
    * tailwind setup
    
    * base colors

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      white: '#F2F1F0',
+      gray: '#AEADAD',
+      black: '#0F0E0C',
+      green: '#4BC2A3',
+      red: '#FF5A44',
+    }
+    extend: {},
+  },
+  plugins: [],
+}
