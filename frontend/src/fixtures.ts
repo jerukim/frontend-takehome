61aed820f45d62d2091ca1bc49dc0f06409e2fe6
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 17:31:59 -0400

    fixture and types

---
+++ frontend/src/fixtures.ts
@@ -0,0 +1,11 @@
+import { Asset } from "./types";
+
+export const asset: Asset = {
+  symbol: "BTC",
+  name: "Bitcoin",
+  price: "31,119.01",
+  "24h_change": "+22.3 USDC (+7.5$)",
+  "1h_funding": "0.00012%",
+  long_open_interest: "8.871 BTC",
+  short_open_interest: "8.871 BTC",
+};
---
+++ frontend/src/types.ts
@@ -0,0 +1,9 @@
+export type Asset = {
+  symbol: string;
+  name: string;
+  price: string;
+  "24h_change": string;
+  "1h_funding": string;
+  long_open_interest: string;
+  short_open_interest: string;
+};
