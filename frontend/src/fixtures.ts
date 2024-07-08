aee19596d498fd4e4a03f2d0edccf59d64ae177a
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-08 19:04:28 -0400

    update fixture data to hourly timestamps with randomized values

--- frontend/src/fixtures.ts
+++ frontend/src/fixtures.ts
@@ -1,4 +1,4 @@
-import { UTCTimestamp } from "lightweight-charts";
+import { CandlestickData, Time, UTCTimestamp } from "lightweight-charts";
 import { Asset } from "./types";
 
 function getRandomArbitrary(min: number, max: number) {
@@ -20,75 +20,27 @@ export const asset: Asset = {
   short_open_interest: parseFloat(getRandomArbitrary(-10, 10).toFixed(3)),
 };
 
-export const series = [
-  {
-    open: 10,
-    high: 10.63,
-    low: 9.49,
-    close: 9.55,
-    time: 1642427876 as UTCTimestamp,
-  },
-  {
-    open: 9.55,
-    high: 10.3,
-    low: 9.42,
-    close: 9.94,
-    time: 1642514276 as UTCTimestamp,
-  },
-  {
-    open: 9.94,
-    high: 10.17,
-    low: 9.92,
-    close: 9.78,
-    time: 1642600676 as UTCTimestamp,
-  },
-  {
-    open: 9.78,
-    high: 10.59,
-    low: 9.18,
-    close: 9.51,
-    time: 1642687076 as UTCTimestamp,
-  },
-  {
-    open: 9.51,
-    high: 10.46,
-    low: 9.1,
-    close: 10.17,
-    time: 1642773476 as UTCTimestamp,
-  },
-  {
-    open: 10.17,
-    high: 10.96,
-    low: 10.16,
-    close: 10.47,
-    time: 1642859876 as UTCTimestamp,
-  },
-  {
-    open: 10.47,
-    high: 11.39,
-    low: 10.4,
-    close: 10.81,
-    time: 1642946276 as UTCTimestamp,
-  },
-  {
-    open: 10.81,
-    high: 11.6,
-    low: 10.3,
-    close: 10.75,
-    time: 1643032676 as UTCTimestamp,
-  },
-  {
-    open: 10.75,
-    high: 11.6,
-    low: 10.49,
-    close: 10.93,
-    time: 1643119076 as UTCTimestamp,
-  },
-  {
-    open: 10.93,
-    high: 11.53,
-    low: 10.76,
-    close: 10.96,
-    time: 1643205476 as UTCTimestamp,
-  },
-];
+
+function generateSeries() {
+  const series: CandlestickData<Time>[] = [];
+
+  for (let i = 0; i < 25; i++) {
+    const prevClose = series[i - 1]?.close ?? asset.price;
+    series.push({
+      open: parseFloat(prevClose.toFixed(2)),
+      high: parseFloat((prevClose + getRandomArbitrary(0, 1000)).toFixed(2)),
+      low: parseFloat((prevClose - getRandomArbitrary(-1000, 0)).toFixed(2)),
+      close: parseFloat(
+        (prevClose + getRandomArbitrary(-1000, 1000)).toFixed(2),
+      ),
+      time:
+        new Date(
+          `2024-06-24T${i.toString().padStart(2, "0")}:00:00Z`,
+        ).getTime() / 1000 as UTCTimestamp,
+    });
+  }
+
+  return series;
+}
+
+export const series = generateSeries();
