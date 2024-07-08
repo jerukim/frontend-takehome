60b39a192dd8122445a0f393c23037a8e796ff9c
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-08 14:34:19 -0400

    color config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-import AssetOverview from "./components/AssetOverview";
-import Chart from "./components/Chart";
-import { Tab, TabItem } from "./components/Tab";
-import Search from "./components/Search";
-
 import {
   CandlestickStyleOptions,
   DeepPartial,
@@ -11,12 +6,19 @@ import {
   TimeChartOptions,
 } from "lightweight-charts";
 
+import AssetOverview from "./components/AssetOverview";
+import Chart from "./components/Chart";
+import PurchaseForm from "./components/PurchaseForm";
+import Search from "./components/Search";
+import { Tab, TabItem } from "./components/Tab";
+
+import tailwind from "./styles/config";
 import { asset, series } from "./fixtures";
 
 const chartOptions: DeepPartial<TimeChartOptions> = {
   autoSize: true,
   watermark: {
-    color: "#F2F1F0",
+    color: tailwind.theme.colors.white,
     visible: true,
     text: "BTC/USDT - 1H - Vest\n",
     fontSize: 14,
@@ -26,7 +28,7 @@ const chartOptions: DeepPartial<TimeChartOptions> = {
   },
   layout: {
     background: { color: "transparent" },
-    textColor: "#AEADAD",
+    textColor: tailwind.theme.colors.gray,
     fontSize: 14,
     fontFamily: "IBM Plex Mono",
   },
@@ -35,10 +37,10 @@ const chartOptions: DeepPartial<TimeChartOptions> = {
   },
   grid: {
     vertLines: {
-      color: "#424242",
+      color: tailwind.theme.colors["black-42"],
     },
     horzLines: {
-      color: "#424242",
+      color: tailwind.theme.colors["black-42"],
     },
   },
 };
@@ -46,13 +48,13 @@ const seriesOptions: DeepPartial<
   CandlestickStyleOptions & SeriesOptionsCommon
 > = {
   priceLineStyle: LineStyle.Solid,
-  priceLineColor: "#4BC2A3",
-  upColor: "#4BC2A3",
-  downColor: "#E03737",
+  priceLineColor: tailwind.theme.colors.green,
+  upColor: tailwind.theme.colors.green,
+  downColor: tailwind.theme.colors["red-e0"],
   borderVisible: false,
-  wickUpColor: "#4BC2A3",
-  wickDownColor: "#E03737",
-  baseLineColor: "#4BC2A3",
+  wickUpColor: tailwind.theme.colors.green,
+  wickDownColor: tailwind.theme.colors["red-e0"],
+  baseLineColor: tailwind.theme.colors.green,
 };
 
 function App() {
