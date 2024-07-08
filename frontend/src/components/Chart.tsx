6782c442df1708e7bc438b3d6ab98048be59f641
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-07 20:44:52 -0400

    chart controls

--- frontend/src/components/Chart.tsx
+++ frontend/src/components/Chart.tsx
@@ -1,6 +1,6 @@
 export default function Chart() {
   return (
-    <div className="col-span-6 grid h-[551px] grid-cols-[auto_1fr] grid-rows-[auto_1fr]">
+    <div className="col-span-6 grid h-[551px] grid-cols-[auto_1fr] grid-rows-[auto_1fr] gap-y-4">
       {/* Chart Settings */}
       <div className="col-span-full col-start-1 row-start-1 flex h-6 items-center justify-between">
         <div className="flex divide-x divide-white">
@@ -36,7 +36,44 @@ export default function Chart() {
       </div>
 
       {/* Chart Controls */}
-      <div className="flex flex-col gap-y-4"></div>
+      <menu className="flex flex-col gap-y-4">
+        <button>
+          <img className="size-6" src="/img/cross.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/trend-line.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/fib ret.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/brush.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/text.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/XABCD pattern.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/long position.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/heart.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/ruler.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/search-zoom-in.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/magnet.svg" alt="" />
+        </button>
+        <button>
+          <img className="size-6" src="/img/lock drawing mode.svg" alt="" />
+        </button>
+      </menu>
 
       {/* Chart */}
       <div className=""></div>
