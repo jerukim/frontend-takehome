006a8c7f3091ef1553203357cad881e7ab6ad128
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-08 20:38:40 -0400

    move series generation to backend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,16 @@
+import { CandlestickData, Time } from "lightweight-charts";
 import { ReactionBody, Reactions } from "../types";
 import { API_BASE_URL } from "./constants";
+
+export async function getSeries() {
+  const res = await fetch(`${API_BASE_URL}/series`);
+
+  if (!res.ok) throw new Error("Something went wrong getting series");
+
+  const data: CandlestickData<Time>[] = await res.json();
+
+  return data;
+}
 
 export async function getReactions() {
   const res = await fetch(`${API_BASE_URL}/getReactions`);
