33a22dd7abec5eae310a4d8d336d137bbf53c768
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-08 22:08:41 -0400

    confetti

--- frontend/src/components/PurchaseForm.tsx
+++ frontend/src/components/PurchaseForm.tsx
@@ -1,9 +1,28 @@
+import confetti from "canvas-confetti";
 import { Tab, TabItem } from "./Tab";
+import { tailwind } from "../styles/config";
+import { triangle } from "../styles/shapes";
 
 export default function PurchaseForm() {
-  function handleClick() {
+  function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     const audio = new Audio("/audio/Ping.wav");
+    const rect = event.currentTarget.getBoundingClientRect();
+
     audio.play();
+    confetti({
+      particleCount: 500,
+      spread: 360,
+      startVelocity: 50,
+      gravity: 2,
+      ticks: 100,
+      colors: [tailwind.theme.colors.green],
+      shapes: [triangle],
+      origin: {
+        x: (rect.x + rect.width / 2) / window.innerWidth,
+        y: (rect.y + rect.height / 2) / window.innerHeight,
+      },
+      scalar: 3,
+    });
   }
 
   return (
---
+++ frontend/src/styles/shapes.ts
@@ -0,0 +1,3 @@
+import confetti from "canvas-confetti";
+
+export const triangle = confetti.shapeFromPath({ path: "M0 10 L5 0 L10 10z" });
